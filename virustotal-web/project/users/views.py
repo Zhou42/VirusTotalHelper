292268d94d0c53889415ec99f8c8f70eadaa67a9
Yang Zhou <zhouyang6828629@gmail.com>
2017-02-20 23:29:22 -0800

    User views and controllers

---
+++ virustotal-web/project/users/views.py
@@ -0,0 +1,95 @@
+# project/users/views.py
+
+
+#################
+#### imports ####
+#################
+
+from flask import render_template, Blueprint, request, redirect, url_for, flash, abort
+from sqlalchemy.exc import IntegrityError
+from flask_login import login_user, current_user, login_required, logout_user
+from threading import Thread
+from itsdangerous import URLSafeTimedSerializer
+from datetime import datetime
+
+from .forms import RegisterForm, LoginForm, EmailForm, PasswordForm
+from project import db, app
+from project.models import User
+
+
+################
+#### config ####
+################
+
+users_blueprint = Blueprint('users', __name__)
+
+
+##########################
+#### helper functions ####
+##########################
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ), 'info')
+
+################
+#### routes ####
+################
+
+@users_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+
+    form = RegisterForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            try:
+                new_user = User(form.email.data, form.password.data)
+
+                new_user.authenticated = True
+                db.session.add(new_user)
+                db.session.commit()
+                # login after register
+                login_user(new_user)
+                flash('Thanks for registering!', 'success')
+                return redirect(url_for('report.upload'))
+
+            except IntegrityError:
+                db.session.rollback()
+                flash('ERROR! Email ({}) already exists.'.format(form.email.data), 'error')
+    return render_template('register.html', form=form)
+
+
+@users_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if user is not None and user.is_correct_password(form.password.data):
+                user.authenticated = True
+                user.last_logged_in = user.current_logged_in
+                user.current_logged_in = datetime.now()
+                db.session.add(user)
+                db.session.commit()
+                login_user(user)
+                flash('Thanks for logging in, {}'.format(current_user.email))
+                return redirect(url_for('report.upload'))
+            else:
+                flash('ERROR! Incorrect login credentials.', 'error')
+    return render_template('login.html', form=form)
+
+
+@users_blueprint.route('/logout')
+@login_required
+def logout():
+    user = current_user
+    user.authenticated = False
+    db.session.add(user)
+    db.session.commit()
+    logout_user()
+    flash('Goodbye!', 'info')
+    return redirect(url_for('users.login'))
